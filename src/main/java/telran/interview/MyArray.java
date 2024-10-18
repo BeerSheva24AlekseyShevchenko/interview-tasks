d4182f39296edf2e86d1f9dde58865ba21ae07dd
AlexShevchenko <aashev4enko@gmail.com>
2024-10-18 20:21:04 +0300

    added MyArray

---
+++ src/main/java/telran/interview/MyArray.java
@@ -0,0 +1,41 @@
+package telran.interview;
+
+import java.util.HashMap;
+
+public class MyArray<T> {
+    T defaultValue = null;
+    HashMap<Integer, T> arr;
+    int size;
+
+    public MyArray(int size) {
+        this.size = size;
+        arr = new HashMap<>();
+    }
+
+    public void setAll(T value) {
+        this.defaultValue = value;
+        arr = new HashMap<>();
+    }
+
+    public void set(int index, T value) {
+        validateIndex(index);
+
+        if (value.equals(defaultValue)) {
+            arr.remove(index);
+        } else {
+            arr.put(index, value);
+        }
+    }
+
+    public T get(int index) {
+        validateIndex(index);
+
+        return arr.containsKey(index) ? arr.get(index) : defaultValue;
+    }
+
+    private void validateIndex (int index) {
+        if (index < 0 || index >= size) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+    }
+}
